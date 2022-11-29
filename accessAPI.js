0584b2e8f1a6992074a1fe10d7421b41ec2c01eb
WMAlves <williamma03@gmail.com>
2022-11-29 07:40:14 -0300

    edição do html. criação do nav.html e do api.js

---
+++ accessAPI.js
@@ -0,0 +1,12 @@
+//excluir
+// utilize isso como base: https://pucminas.instructure.com/courses/127501/pages/videoaula-restful-api?module_item_id=2817882
+   
+   fetch("https://api.rawg.io/api/games?key=68ba723ff4a1495baf4efb38a8276320", { method: "GET",})
+   .then(res=>res.json());
+
+
+
+
+
+   
+   
