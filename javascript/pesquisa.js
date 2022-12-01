dca28a1a19dc93fb96aa34155f8f6fcbb940d46d
Gabriel Moura <gabriel.moura_hartbgroup@prestador.globo>
2022-12-01 18:05:49 -0300

    fix: fixed div name on innerHTML call

--- javascript/pesquisa.js
+++ javascript/pesquisa.js
@@ -1,19 +1,19 @@
-const apiKey = '68ba723ff4a1495baf4efb38a8276320'
-let txtSearch=document.getElementById('txtSearch')
-let btnSearch=document.getElementById('btnSearch')
-let gamesPlace=document.getElementById('exibePesquisa')
+const apiKey = "68ba723ff4a1495baf4efb38a8276320";
+let txtSearch = document.getElementById("txtSearch");
+let btnSearch = document.getElementById("btnSearch");
+let gamesPlace = document.getElementById("exibePesquisa");
 
-function doSearch(){
-    let textoPesquisa=txtSearch.value 
-    let url = `https://api.rawg.io/api/games?key=${apiKey}&search=${textoPesquisa}`
-    
-    fetch(url)
-    .then(res => res.json())
-    .then(data=>{
-        var exibe=''
-        for(let i=0; i< data.results.length;i++){
-            let game= data.results[i]
-         exibe+=`<div class="col 12 col-sm-12 col-md-6 col-lg-3">
+function doSearch() {
+  let textoPesquisa = txtSearch.value;
+  let url = `https://api.rawg.io/api/games?key=${apiKey}&search=${textoPesquisa}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      var exibe = "";
+      for (let i = 0; i < data.results.length; i++) {
+        let game = data.results[i];
+        exibe += `<div class="col 12 col-sm-12 col-md-6 col-lg-3">
             <img src="${game.background_image}" class="card-img-top">
               <div class="card" style="width: 18r em;">                         
                 <div class="card-body">
@@ -23,17 +23,14 @@ function doSearch(){
                   <a href="detalhes.html?${game.id}" class="btn btn-secondary" >Ver mais</a>
                 </div>
               </div>
-          </div> `
-        }
-        document.getElementById('exibePesquisa').innerHTML = exibe;
-    })
-
-
+          </div> `;
+      }
+      document.getElementById("tela").innerHTML = exibe;
+    });
 
   //alert(textoPesquisa)
-    
 }
 
-document.body.onload=()=>{
-    btnSearch.addEventListener('click',doSearch) 
-} 
+document.body.onload = () => {
+  btnSearch.addEventListener("click", doSearch);
+};
